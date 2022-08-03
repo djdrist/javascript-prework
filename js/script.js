c86f8abe08a50ad922a51f471431fdff577cf27d
Lukasz Kallas <lukasz.kallas@gmail.com>
2022-08-03 10:28:35 +0200

    Simpler displayResult

--- js/script.js
+++ js/script.js
@@ -15,34 +15,26 @@ function getMoveName(argMoveId){
 function displayResult(argComputerMove, argPlayerMove){
     printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
 
-    if( argComputerMove == 'kamień' && argPlayerMove == 'kamień'){
+    if( argComputerMove == argPlayerMove ){
         printMessage('Jest remis!');
-    } else if( argComputerMove == 'kamień' && argPlayerMove == 'papier'){
+    } else if(argComputerMove == 'kamień' && argPlayerMove == 'papier'){
         printMessage('Ty wygrywasz!');
-    } else if( argComputerMove == 'kamień' && argPlayerMove == 'nożyce'){
+    } else if(argComputerMove == 'kamień' && argPlayerMove == 'nożyce'){
         printMessage('Komputer wygrywa!');
-    } else if( argComputerMove == 'kamień' && argPlayerMove == 'nieznany ruch'){
+    } else if(argPlayerMove == 'nieznany ruch'){
         printMessage('Nie dokonałeś wyboru!');
     } 
 
-    if( argComputerMove == 'papier' && argPlayerMove == 'papier'){
-        printMessage('Jest remis!');
-    } else if( argComputerMove == 'papier' && argPlayerMove == 'nożyce'){
+    if(argComputerMove == 'papier' && argPlayerMove == 'nożyce'){
         printMessage('Ty wygrywasz!');
-    } else if( argComputerMove == 'papier' && argPlayerMove == 'kamień'){
+    } else if(argComputerMove == 'papier' && argPlayerMove == 'kamień'){
         printMessage('Komputer wygrywa!');
-    } else if( argComputerMove == 'papier' && argPlayerMove == 'nieznany ruch'){
-        printMessage('Nie dokonałeś wyboru!');
     } 
 
-    if( argComputerMove == 'nożyce' && argPlayerMove == 'nożyce'){
-        printMessage('Jest remis!');
-    } else if( argComputerMove == 'nożyce' && argPlayerMove == 'kamień'){
+    if(argComputerMove == 'nożyce' && argPlayerMove == 'kamień'){
         printMessage('Ty wygrywasz!');
-    } else if( argComputerMove == 'nożyce' && argPlayerMove == 'papier'){
+    } else if(argComputerMove == 'nożyce' && argPlayerMove == 'papier'){
         printMessage('Komputer wygrywa!');
-    } else if( argComputerMove == 'nożyce' && argPlayerMove == 'nieznany ruch'){
-        printMessage('Nie dokonałeś wyboru!');
     }
 }
 
